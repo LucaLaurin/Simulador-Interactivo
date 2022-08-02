10f9d7b106f3264132b99c2dae65a87aacde1fee
Luca Laurin <lucalaurin23@gmail.com>
2022-08-01 23:39:46 -0300

    cambios

---
+++ sweetAlert.js
@@ -0,0 +1,10 @@
+Swal.fire({
+    title: 'Bienvenido!',
+    Text: 'espero que te guste la calculadora',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
